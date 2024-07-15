e5a84e17f37ed87f3229314f7b708093ed88f38a
Samuel Belo <belo.samuel@gmail.com>
2024-07-15 10:44:20 -0300

    cors config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors();
     const config = new DocumentBuilder()
         .setTitle('Open Education API')
         .setDescription('API for Open Education web application')
